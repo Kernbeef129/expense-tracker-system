876685fb6efe82911af1269b1a54898d18add6d6
Kernbeef129 <tejano.johnmarsansen1232@gmail.com>
2026-05-08 13:13:23 +0800

    Initial Expense Tracker System

---
+++ # Expense Tracker System.py
@@ -0,0 +1,35 @@
+# Expense Tracker System
+
+# -----------------------------
+# Functions
+# -----------------------------
+
+def add_expense():
+    pass
+
+
+def delete_expense():
+    pass
+
+
+def clear_expenses():
+    pass
+
+
+def update_total():
+    pass
+
+
+def save_data():
+    pass
+
+
+def load_data():
+    pass
+
+
+# -----------------------------
+# Main Program
+# -----------------------------
+
+print("Expense Tracker System Started")
